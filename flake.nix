61c23daa5aba39fcabdd1eca32e4a5f9af18f9bf
Taeer Bar-Yam <taeer@bar-yam.me>
2025-11-11 01:13:36 +0100

    complete makeover
    this repo is now solely a template, not a library

--- flake.nix
+++ flake.nix
@@ -2,12 +2,12 @@
   description = "Hakyll Website";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-23.05";
+    nixpkgs.url = "nixpkgs/nixos-25.05";
 
-    # these aren't needed in 23.05, but often a few dependencies have broken
+    # these aren't needed in 25.05, but often a few dependencies have broken
     # versions. this (plus the commented out code below)  is the way one would
     # fix them
-    
+
     # hakyll-src = {
     #   url = "github:jaspervdj/hakyll/v4.13.4.1";
     #   flake = false;
@@ -19,42 +19,81 @@
   };
 
   outputs = { self, nixpkgs
-  # , hakyll-src, hakyll-sass-src 
+  # , hakyll-src, hakyll-sass-src
   }:
     let
-      # taken from nixpkgs flake.nix
-      systems = [
-        "x86_64-linux"
-        "i686-linux"
-        "x86_64-darwin"
-        "aarch64-linux"
-        "armv6l-linux"
-        "armv7l-linux"
+      system = "x86_64-linux";
+      inherit (nixpkgs) lib;
+      pkgs = nixpkgs.legacyPackages.${system};
+
+      # overlay = self: super: {
+      #   haskellPackages = super.haskellPackages.extend (hsSelf: hsSuper: {
+      #     hakyll = hsSuper.callCabal2nix "hakyll" "${hakyll-src}" { };
+      #     hakyll-sass = hsSuper.callCabal2nix "hakyll-sass" "${hakyll-sass-src}" { };
+      #   });
+      # };
+      # overlays = [ overlay ];
+      # pkgs = import nixpkgs { inherit system overlays; };
+
+      inherit (pkgs) stdenv glibcLocales mkShell haskellPackages;
+      name = "my-website";
+      src = lib.fileset.toSource {
+        root = ./.;
+        fileset = lib.fileset.unions [
+          ./src
+          ./site.hs
+          ./package.yaml
+        ];
+      };
+      builder = haskellPackages.callCabal2nix name src {};
+      haskell-env = pkgs.ghc.withHoogle (hp: with hp; [ haskell-language-server cabal-install ] ++ builder.buildInputs);
+      websiteBuildInputs = with pkgs; [
+        # other inputs you need to build the website. e.g.
+        # rubber
+        # texliveFull
+        # poppler_utils
       ];
-
-      # taken from nixpkgs flake.nix
-      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
-
-      overlay = (self: super: {
-        haskellPackages = super.haskellPackages.extend (hsSelf: hsSuper: {
-          # hakyll = hsSuper.callCabal2nix "hakyll" "${hakyll-src}" { };
-          # hakyll-sass = hsSuper.callCabal2nix "hakyll-sass" "${hakyll-sass-src}" { };
-        });
-      });
-
-      overlays = [ overlay ];
-
-      # this is just nixpkgs.legacyPackages but with an overlay
-      pkgs = forAllSystems (system: import nixpkgs { inherit system overlays; });
-    in
-      {
-        inherit pkgs overlay overlays;
-        lib = rec {
-          mkBuilderPackage = args: (mkAllOutputs args).packages.builder;
-          mkWebsitePackage = args: (mkAllOutputs args).packages.website;
-          mkDevShell       = args: (mkAllOutputs args).devShell;
-          mkApp            = args: (mkAllOutputs args).defaultApp;
-          mkAllOutputs = import ./gen.nix pkgs.${system};
+    in {
+      packages.${system} = rec {
+        inherit builder;
+        default = website;
+        website = stdenv.mkDerivation {
+          inherit name src;
+          buildInputs = [ builder ] ++ websiteBuildInputs;
+          LANG = "en_US.UTF-8";
+          LC_ALL = "en_US.UTF-8";
+          LOCALE_ARCHIVE = "${glibcLocales}/lib/locale/locale-archive";
+          # don't look in fcaches for this; speeds things up a little
+          allowSubstitutes = false;
+          buildPhase = ''
+            ${name} build
+          '';
+          installPhase = ''
+            cp -R _site $out
+          '';
+          dontStrip = true;
         };
       };
+      devShells.${system}.default = mkShell {
+        name = "${name}-env";
+        buildInputs = [ haskell-env ] ++ websiteBuildInputs;
+
+        shellHook = ''
+          export HAKYLL_ENV="development"
+
+          export HIE_HOOGLE_DATABASE="${haskell-env}/share/doc/hoogle/default.hoo"
+          export NIX_GHC="${haskell-env}/bin/ghc"
+          export NIX_GHCPKG="${haskell-env}/bin/ghc-pkg"
+          export NIX_GHC_DOCDIR="${haskell-env}/share/doc/ghc/html"
+          export NIX_GHC_LIBDIR=$( $NIX_GHC --print-libdir )
+        '';
+      };
+      apps.${system}.default = {
+        type = "app";
+        program = builder + "/bin/${name}";
+      };
+      templates.default = {
+        path = ./.;
+      };
+    };
 }
--- readme.md
+++ readme.md
@@ -1,70 +1,10 @@
 # hakyll-flakes
 
-Use this to easily build your hakyll websites using nix flakes.
+I've realised that this project was super over-engineered. The new design is a template that reflects how I build my hakyll project. To use it, copy the `flake.nix` into your own project or adapt it to fit with what you already have in your `flake.nix`. You can take inspiration also from the rest of the repository if it's helpful, though it was mostly just generated with `hakyll-init`. If you want to start a fresh project, it may be useful to run `nix flake init -t github:radvendii/hakyll-flakes` in an empty directory.
 
-`hakyll-flakes.lib.mk*` takes in a set with the following fields:
-
-- `system`: the system to build for.
-- `name`: the name of website. this must also be the name of the haskell project and executable generated.
-- `src`: the source directory (usually `./.`), which must contain at a minimum your `package.yaml` or `project-name.cabal` file.
-- `websiteBuildInputs` (optional): any runtime inputs the builder needs to build the website.
-
-`hakyll-flakes.overlay` is the overlay that `hakyll-flakes` uses internally to get hakyll working (this will not work on an arbitrary version of nixpkgs), and `hakyll-flake.pkgs` is the legacyPackages that `hakyll-flakes` uses internally (this should always work). If you want to use one consistent nixpkgs set, you can set `websiteBuildInputs = with hakyll-flakes.pkgs.${system}; [ ... ]`. Alternatively, you can use your own nixpkgs set for these inputs, there's no reason they need to be the same.
-
-# Example
-
-## `flake.nix`
-```
-{
-  description = "My Website";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs";
-  inputs.hakyll-flakes.url = "github:Radvendii/hakyll-flakes";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-
-  outputs = { self, hakyll-flakes, flake-utils, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      hakyll-flakes.lib.mkAllOutputs {
-        inherit system;
-        name = "my-website";
-        src = ./.;
-        websiteBuildInputs = with nixpkgs.legacyPackages.${system}; [
-          rubber
-          texlive.combined.scheme-full
-          poppler_utils
-        ];
-      }
-    );
-}
-```
-
-## `package.yaml`
-```
-name: my-website
-
-dependencies:
-    - base
-    - hakyll
-    - filepath
-    - pandoc
-    - containers
-    - time
-    - pandoc-types
-    - hakyll-sass
-    - process
-    - bytestring
-    - uri-encode
-    - text
-    - time-locale-compat
-
-executable:
-    main: site.hs
-```
-
-Then from the top level directory you can run a few different commands:
+From the top level directory you can run a few different commands:
 
 - `nix build .#website` (or just `nix build`) this goes through the whole process for you and simply produces a `result/` symlink with your compiled website inside.
 - `nix build .#builder` this builds the website *builder*, your hakyll-based `site.hs` file. `You can find it at result/bin/<project name>`.
-- `nix run . -- watch` This will compile and run your website builder, creating the `_site` directory and loading the website at `localhost:8000`. It will also rebuild the website if you change the files (but not if you change `site.hs`)
-
-You can pick out the individual outputs you want with `mkBuilderPackage`, `mkWebsitePackage`, `mkDevShell`, and `mkApp`.
+- `nix run -- watch` This will compile and run your website builder, creating the `_site` directory and loading the website at `localhost:8000`. It will also rebuild the website if you change the files (but not if you change `site.hs`)
+- `nix develop` - this will provide you with the inputs you need to build the website in your `$PATH`. Also gives you what you need to set up the haskell LSP for your editor with all of your packages available.
